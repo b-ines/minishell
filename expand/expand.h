88e91a3a347e8f32fe368d8837ebed4b948995b2
Gabriel Chalmel Toussaint <gchalmel@f2r10s1.clusters.42paris.fr>
2026-02-17 15:32:25 +0100

    add expand function in header file

--- expand/expand.h
+++ expand/expand.h
@@ -6,7 +6,7 @@
 /*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 14:39:40 by gchalmel          #+#    #+#             */
-/*   Updated: 2026/02/17 14:41:19 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/02/17 15:20:02 by gchalmel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include "../libft/libft.h"
 # include "../lexer/lexer.h"
 
-
+void expand(t_token *token);
 
 #endif
