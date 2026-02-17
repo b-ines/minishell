48e72408927477dbcbbf425632bca18dc48a72eb
Gabriel Chalmel Toussaint <gchalmel@f2r10s1.clusters.42paris.fr>
2026-02-17 15:15:35 +0100

    Make expand (no test)

--- expand/expand.c
+++ expand/expand.c
@@ -6,11 +6,13 @@
 /*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 14:40:10 by gchalmel          #+#    #+#             */
-/*   Updated: 2026/02/17 15:07:16 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/02/17 15:14:53 by gchalmel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expand.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 static int	is_expand(t_token token)
 {
@@ -24,12 +26,21 @@ static int	is_expand(t_token token)
 	return (0);
 }
 
+static void	make_expand(t_token *token)
+{
+	char	*var;
+
+	var = getenv(token->token);
+	free(token->token);
+	token->token = var;
+}
+
 void	expand(t_token *token)
 {
 	while (token != NULL)
 	{
 		if (is_expand(*token))
-			return ;
+			make_expand(token);
 		token = token->next;
 	}
 }
