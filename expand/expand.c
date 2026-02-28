3a5e5d1f0446b34d7f5a9fb5346cee3e6b73afa6
Gabriel Chalmel Toussaint <gchalmel@f4r5s9.clusters.42paris.fr>
2026-02-28 15:13:29 +0100

    try to commit expand

--- expand/expand.c
+++ expand/expand.c
@@ -6,7 +6,7 @@
 /*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 14:40:10 by gchalmel          #+#    #+#             */
-/*   Updated: 2026/02/24 17:48:38 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/02/28 15:13:10 by gchalmel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static t_expand_ctx is_expand(t_token token) {
-  int i;
+static t_expand_ctx	is_expand(t_token token)
+{
+	int	i;
 
-  i = 0;
-  if (token.quote_flag == 1)
-    return ((t_expand_ctx){i, NONE});
-  while (token.token[i] != '\0') {
-    if (token.type == WORD) {
-      if (token.token[i] == '$' && ft_isalpha(token.token[i + 1]))
-        return ((t_expand_ctx){i + 1, ENV});
-      else if (token.token[i] == '$' && token.token[i + 1] == '$')
-        return ((t_expand_ctx){i + 1, ENV});
-      else if (token.token[i] == '$' && token.token[i + 1] == '?')
-        return ((t_expand_ctx){i + 1, EXIT_STATUS});
-    }
-    i++;
-  }
-  printf("expand inconn\n");
-  return ((t_expand_ctx){i, NONE});
+	i = 0;
+	if (token.quote_flag == 1)
+		return ((t_expand_ctx){i, NONE});
+	while (token.token[i] != '\0')
+	{
+		if (token.type == WORD)
+		{
+			if (token.token[i] == '$' && ft_isalpha(token.token[i + 1]))
+				return ((t_expand_ctx){i + 1, ENV});
+			else if (token.token[i] == '$' && token.token[i + 1] == '$')
+				return ((t_expand_ctx){i + 1, ENV});
+			else if (token.token[i] == '$' && token.token[i + 1] == '?')
+				return ((t_expand_ctx){i + 1, EXIT_STATUS});
+		}
+		i++;
+	}
+	printf("expand inconn\n");
+	return ((t_expand_ctx){i, NONE});
 }
 
-void expand(t_token **token, t_terminal term) {
-  t_expand_ctx ctx;
-  t_token *curr;
+void	expand(t_token **token, t_terminal term)
+{
+	t_expand_ctx	ctx;
+	t_token			*curr;
 
-  printf("Step to expand\n");
-  curr = *token;
-  while (curr != NULL) {
-    ctx = is_expand(*curr);
-    if (ctx.ex_type == ENV)
-      curr = make_expand_env(token, curr, ctx.index, term.envp);
-    else if (ctx.ex_type == EXIT_STATUS)
-      make_exit_status(curr, term);
-    else
-      curr = curr->next;
-  }
+	printf("Step to expand\n");
+	curr = *token;
+	while (curr != NULL)
+	{
+		ctx = is_expand(*curr);
+		if (ctx.ex_type == ENV)
+			curr = make_expand_env(token, curr, ctx.index, term.envp);
+		else if (ctx.ex_type == EXIT_STATUS)
+			make_exit_status(curr, term);
+		else
+			curr = curr->next;
+	}
 }
--- expand/expand_type.c
+++ expand/expand_type.c
@@ -6,7 +6,7 @@
 /*   By: gchalmel <gchalmel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 16:42:33 by gchalmel          #+#    #+#             */
-/*   Updated: 2026/02/24 17:48:16 by gchalmel         ###   ########.fr       */
+/*   Updated: 2026/02/28 15:09:33 by gchalmel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,89 +14,89 @@
 #include "../libft/libft.h"
 #include "expand.h"
 
-void retokenize(t_token **token, t_token *curr, char *final_token,
-                t_token *ret_node)
+void	retokenize(t_token **token, t_token *curr, char *final_token,
+				t_token *ret_node)
 {
-    char **split_token;
-    t_token	*token_new;
-    t_token	*token_cpy;
-    int i;
-    t_token	*head;
+	char	**split_token;
+	t_token	*token_new;
+	t_token	*token_cpy;
+	int		i;
+	t_token	*head;
 
-    token_new = NULL;
-    split_token = ft_split(final_token, ' ');
-    i = 1;
-    ft_addback(&token_new, split_token[0], 0);
-    while (split_token[i] != NULL)
-    {
-        ft_addback(&token_new, " ", 0);
-        ft_addback(&token_new, split_token[i++], 0);
-    }
-    set_type(&token_new);
-    token_cpy = curr->next;
-    if (curr->prev != NULL)
-    {
-        curr = curr->prev;
-        // free((*token)->next->token);
-        // free((*token)->next);
-        curr->next = NULL;
-        curr->next = token_new;
-        token_new->prev = curr;
-        ret_node = NULL;
-    }
-    else
-    {
-        (*token) = token_new;
-        ret_node = NULL;
-    }
-    if (token_cpy != NULL)
-    {
-        head = *token;
-        while (head->next != NULL)
-            head = head->next;
-        head->next = token_cpy;
-        token_cpy->prev = head;
-        ret_node = token_cpy;
-    }
+	token_new = NULL;
+	split_token = ft_split(final_token, ' ');
+	i = 1;
+	ft_addback(&token_new, split_token[0], 0);
+	while (split_token[i] != NULL)
+	{
+		ft_addback(&token_new, " ", 0);
+		ft_addback(&token_new, split_token[i++], 0);
+	}
+	set_type(&token_new);
+	token_cpy = curr->next;
+	if (curr->prev != NULL)
+	{
+		curr = curr->prev;
+		// free((*token)->next->token);
+		// free((*token)->next);
+		curr->next = NULL;
+		curr->next = token_new;
+		token_new->prev = curr;
+		ret_node = NULL;
+	}
+	else
+	{
+		(*token) = token_new;
+		ret_node = NULL;
+	}
+	if (token_cpy != NULL)
+	{
+		head = *token;
+		while (head->next != NULL)
+			head = head->next;
+		head->next = token_cpy;
+		token_cpy->prev = head;
+		ret_node = token_cpy;
+	}
 }
 
 t_token	*make_expand_env(t_token **token, t_token *curr, int index, char **envp)
 {
-    char *var;
-    char *final_token;
-    int len_before_dollar;
-    size_t len_var;
-    t_token	*ret_node;
+	char	*var;
+	char	*final_token;
+	int		len_before_dollar;
+	size_t	len_var;
+	t_token	*ret_node;
 
-    printf("Expand detected on: %s\n", curr->token);
-    var = ft_getenv(envp, &curr->token[index]);
-    if (index > 1)
-    {
-        if (var == NULL)
-            len_var = 0;
-        else
-            len_var = ft_strlen(var);
-        len_before_dollar = ft_strlen_sep(curr->token, '$');
-        final_token = malloc(sizeof(char) * len_before_dollar + len_var + 1);
-        ft_strlcpy(final_token, curr->token, len_before_dollar + 1);
-        if (var != NULL)
-            ft_strlcat(final_token, var, len_before_dollar + len_var + 1);
-    }
-    else
-        final_token = var;
-    if (curr->quote_flag == 0)
-        retokenize(token, curr, final_token, ret_node);
-    else
-    {
-        free(curr->token);
-        curr->token = final_token;
-        ret_node = (curr->next);
-    }
-    return (ret_node);
+	printf("Expand detected on: %s\n", curr->token);
+	var = ft_getenv(envp, &curr->token[index]);
+	if (index > 1)
+	{
+		if (var == NULL)
+			len_var = 0;
+		else
+			len_var = ft_strlen(var);
+		len_before_dollar = ft_strlen_sep(curr->token, '$');
+		final_token = malloc(sizeof(char) * len_before_dollar + len_var + 1);
+		ft_strlcpy(final_token, curr->token, len_before_dollar + 1);
+		if (var != NULL)
+			ft_strlcat(final_token, var, len_before_dollar + len_var + 1);
+	}
+	else
+		final_token = var;
+	if (curr->quote_flag == 0)
+		retokenize(token, curr, final_token, ret_node);
+	else
+	{
+		free(curr->token);
+		curr->token = final_token;
+		ret_node = (curr->next);
+	}
+	return (ret_node);
 }
 
-void make_exit_status(t_token *token, t_terminal term)
+void	make_exit_status(t_token *token, t_terminal term)
 {
-    free(token->token);
-    token->token = ft_itoa(term.exit_status);
+	free(token->token);
+	token->token = ft_itoa(term.exit_status);
 }
